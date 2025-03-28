4f13eea8335ee21d6d6115813cf0ca2f082d0786
anish4463 <145772077+anish4463@users.noreply.github.com>
2025-03-28 22:29:59 +0530

    Delete resume-screener directory

--- resume-screener/grammar_checker.py
+++
@@ -1,8 +0,0 @@
-import language_tool_python
-
-tool = language_tool_python.LanguageTool("en-US")
-
-def check_grammar(text):
-    """Return the number of grammar errors in a resume."""
-    matches = tool.check(text)
-    return len(matches)
